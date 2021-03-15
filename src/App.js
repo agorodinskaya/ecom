3568c90e66507859ad5e829cd04d8dabe41a621a
Anastasia Gorodinskaya <agorodinskaya@gmail.com>
2021-03-16 08:48:34 +1100

    project start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+function App() {
+  return (
+    <div className='App'>
+      <NavBar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
